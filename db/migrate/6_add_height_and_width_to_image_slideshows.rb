762fc92ecace16d851639614a5905c8f2026f57f
Dan Mitchell <dan@digitalreflow.co.uk>
2014-02-03 22:22:25 +0000

    remove height and width fields from slides

--- db/migrate/6_add_height_and_width_to_image_slideshows.rb
+++
@@ -1,6 +0,0 @@
-class AddHeightAndWidthToImageSlideshows < ActiveRecord::Migration
-  def change
-    add_column :refinery_image_slideshows, :height, :string
-    add_column :refinery_image_slideshows, :width, :string
-  end
-end
