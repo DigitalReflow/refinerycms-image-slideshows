e0dba31fbe59d2ef118052af52cf40319fe407a2
Dan Mitchell <dan@digitalreflow.co.uk>
2014-02-03 22:32:11 +0000

    update accessible attributes.

--- app/models/refinery/image_slideshows/image_slide.rb
+++ app/models/refinery/image_slideshows/image_slide.rb
@@ -3,7 +3,7 @@ module Refinery
     class ImageSlide < Refinery::Core::BaseModel
       self.table_name = 'refinery_image_slides'
 
-      attr_accessible :title, :image_id, :caption, :link_url, :position, :image_slideshow_id
+      attr_accessible :title, :title_as_image_id, :title_as_image_position, :image_id, :caption, :caption_position, :position, :image_slideshow_id
 
       acts_as_indexed :fields => [:title]
 
@@ -13,7 +13,6 @@ module Refinery
       belongs_to :image_slideshow
       belongs_to :image
 
-      delegate :height, :width, :to => :image_slideshow
     end
   end
 end
