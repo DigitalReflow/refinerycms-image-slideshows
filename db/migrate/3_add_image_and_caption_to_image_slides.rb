0eb79cedaa95c42074a4dd5dc1c8347a58be0582
Dan Mitchell <dan@digitalreflow.co.uk>
2014-02-03 23:52:21 +0000

    change caption to text field

--- db/migrate/3_add_image_and_caption_to_image_slides.rb
+++ db/migrate/3_add_image_and_caption_to_image_slides.rb
@@ -1,7 +1,7 @@
 class AddImageAndCaptionToImageSlides < ActiveRecord::Migration
   def change
     add_column :refinery_image_slides, :image_id, :integer
-    add_column :refinery_image_slides, :caption, :string
+    add_column :refinery_image_slides, :caption, :text
     add_column :refinery_image_slides, :caption_position, :string
   end
 end
