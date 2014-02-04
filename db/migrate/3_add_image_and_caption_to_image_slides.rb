90f7e2116aea6ab35b422c846faa812dd44a81da
Dan Mitchell <dan@digitalreflow.co.uk>
2014-02-04 23:32:12 +0000

    remove caption_position and replace with caption_color from migration

--- db/migrate/3_add_image_and_caption_to_image_slides.rb
+++ db/migrate/3_add_image_and_caption_to_image_slides.rb
@@ -2,6 +2,6 @@ class AddImageAndCaptionToImageSlides < ActiveRecord::Migration
   def change
     add_column :refinery_image_slides, :image_id, :integer
     add_column :refinery_image_slides, :caption, :text
-    add_column :refinery_image_slides, :caption_position, :string
+    add_column :refinery_image_slides, :caption_color, :string
   end
 end
