3135ef9d56bdc4e0818fa5fa0e7e8b4674e027c0
Paul Groves <paul@grov.es>
2014-02-12 19:29:01 +0000

    Adding mobile and tablet image fields

--- app/models/refinery/image_slideshows/image_slide.rb
+++ app/models/refinery/image_slideshows/image_slide.rb
@@ -3,7 +3,7 @@ module Refinery
     class ImageSlide < Refinery::Core::BaseModel
       self.table_name = 'refinery_image_slides'
 
-      attr_accessible :title, :title_as_image_id, :title_as_image_position, :image_id, :caption, :caption_color, :position, :image_slideshow_id
+      attr_accessible :title, :title_as_image_id, :title_as_image_position, :tablet_image_id, :mobile_image_id, :image_id, :caption, :caption_color, :position, :image_slideshow_id
 
       acts_as_indexed :fields => [:title]
 
@@ -12,6 +12,8 @@ module Refinery
 
       belongs_to :image_slideshow
       belongs_to :image, :class_name => '::Refinery::Image'
+      belongs_to :tablet_image, :class_name => '::Refinery::Image'
+      belongs_to :mobile_image, :class_name => '::Refinery::Image'
       belongs_to :title_as_image, :class_name => '::Refinery::Image'
 
       default_scope order('position asc')
